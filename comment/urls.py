688bb2066e06a481f17d0dea658e8bad3ef5c262
OmuraCode <akyl.net@gmail.com>
2023-07-16 22:13:04 +0600

    added comments

--- comment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from comment import views
-
-
-urlpatterns = [
-    path('', views.CommentCreateView.as_view()),
-    path('<int:pk>/', views.CommentDetailView.as_view()),
-]
-
