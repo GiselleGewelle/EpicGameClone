b99e6c562e3760630b83e68cc7e3b3612a18f4af
GiselleGewelle <satavamas@mail.ru>
2023-07-17 23:15:35 +0600

    for

--- config/settings.py
+++ config/settings.py
@@ -238,22 +238,3 @@ SWAGGER_SETTINGS = {
 #     'http://localhost:3000',
 #
 # ]
-
-
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]
-
-CORS_ALLOW_METHODS = [
-    'GET',
-    'POST',
-    'OPTIONS',
-]
-
-CORS_ALLOW_HEADERS = [
-    'content-type',
-    'authorization',
-]
-
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = False
